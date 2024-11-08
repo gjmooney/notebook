39612bd39ad79bd2d10be1d1c267d1ca2383d37b
Greg <gjmooney@asu.edu>
2024-11-08 15:44:46 +0100

    save

--- app/bak_webpack.config.js
+++ app/bak_webpack.config.js
@@ -18,19 +18,12 @@ const WPPlugin = require('@jupyterlab/builder').WPPlugin;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const baseConfig = require('@jupyterlab/builder/lib/webpack.config.base');
 
-const topLevelData = require('./package.json');
-const liteAppData = topLevelData.jupyterlite.apps.reduce(
-  (memo, app) => ({ ...memo, [app]: require(`./${app}/package.json`) }),
-  {}
-);
+const data = require('./package.json');
 
-// liteAppData is an object where each key is an app name and the vale are the contents of its package.json
-// console.log('liteAppData', liteAppData);
-
-// const names = Object.keys(topLevelData.dependencies).filter((name) => {
-//   const packageData = require(path.join(name, 'package.json'));
-//   return packageData.jupyterlab !== undefined;
-// });
+const names = Object.keys(data.dependencies).filter((name) => {
+  const packageData = require(path.join(name, 'package.json'));
+  return packageData.jupyterlab !== undefined;
+});
 
 // Ensure a clear build directory.
 const buildDir = path.resolve(__dirname, 'build');
@@ -38,6 +31,18 @@ if (fs.existsSync(buildDir)) {
   fs.removeSync(buildDir);
 }
 fs.ensureDirSync(buildDir);
+
+// Handle the extensions.
+const { mimeExtensions, plugins } = data.jupyterlab;
+
+// Create the list of extension packages from the package.json metadata
+const extensionPackages = new Set();
+Object.keys(plugins).forEach((page) => {
+  const pagePlugins = plugins[page];
+  Object.keys(pagePlugins).forEach((name) => {
+    extensionPackages.add(name);
+  });
+});
 
 Handlebars.registerHelper('json', (context) => {
   return JSON.stringify(context);
@@ -69,13 +74,37 @@ Handlebars.registerHelper('list_plugins', () => {
   return str;
 });
 
+// Create the entry point and other assets in build directory.
+const source = fs.readFileSync('index.template.js').toString();
+const template = Handlebars.compile(source);
+const extData = {
+  notebook_plugins: plugins,
+  notebook_mime_extensions: mimeExtensions,
+};
+const indexOut = template(extData);
+fs.writeFileSync(path.join(buildDir, 'index.js'), indexOut);
+
+// Make a bootstrap entrypoint
+const entryPoint = path.join(buildDir, 'bootstrap.js');
+const bootstrap = 'import("./index.js");';
+fs.writeFileSync(entryPoint, bootstrap);
+
+// Copy extra files
+const cssImports = path.resolve(__dirname, 'style.js');
+fs.copySync(cssImports, path.resolve(buildDir, 'extraStyle.js'));
+
+const extras = Build.ensureAssets({
+  packageNames: names,
+  output: buildDir,
+  schemaOutput: path.resolve(__dirname, '..', 'notebook'),
+});
+
 /**
  * Create the webpack ``shared`` configuration
  */
 function createShared(packageData) {
   // Set up module federation sharing config
   const shared = {};
-  const extensionPackages = packageData.jupyterlab.extensions;
 
   // Make sure any resolutions are shared
   for (let [pkg, requiredVersion] of Object.entries(packageData.resolutions)) {
@@ -169,115 +198,6 @@ function createShared(packageData) {
   return shared;
 }
 
-// app/build
-const topLevelBuild = path.resolve('build');
-
-const allAssetConfig = [];
-const allEntryPoints = {};
-const allHtmlPlugins = [];
-
-for (const [name, data] of Object.entries(liteAppData)) {
-  const buildDir = path.join(name, 'build');
-
-  const packageNames = data.jupyterlab.extensions;
-  // Generate webpack config to copy extension assets to the build directory,
-  // such as setting schema files, theme assets, etc.
-  const extensionAssetConfig = Build.ensureAssets({
-    packageNames,
-    output: buildDir,
-    schemaOutput: path.resolve(__dirname, '..', 'notebook'),
-    // themeOutput: topLevelBuild,
-  });
-
-  allAssetConfig.push(extensionAssetConfig);
-
-  // Create a list of application extensions and mime extensions from
-  // jlab.extensions
-  const extensions = {};
-  const mimeExtensions = {};
-  for (const key of packageNames) {
-    const {
-      jupyterlab: { extension, mimeExtension },
-    } = require(`${key}/package.json`);
-    if (extension !== undefined) {
-      extensions[key] = extension === true ? '' : extension;
-    }
-    if (mimeExtension !== undefined) {
-      mimeExtensions[key] = mimeExtension === true ? '' : mimeExtension;
-    }
-  }
-
-  // Retrieve app info from package.json
-  const { appClassName, appModuleName, disabledExtensions } = data.jupyterlab;
-
-  // Create the entry point and other assets in build directory.
-  const template = Handlebars.compile(
-    fs.readFileSync(path.resolve('./index.template.js')).toString()
-  );
-  fs.writeFileSync(
-    path.join(name, 'build', 'index.js'),
-    template({
-      name,
-      appClassName,
-      appModuleName,
-      extensions,
-      mimeExtensions,
-      disabledExtensions,
-    })
-  );
-  // Create the bootstrap file that loads federated extensions and calls the
-  // initialization logic in index.js
-  const entryPoint = `./${name}/build/bootstrap.js`;
-  fs.copySync('bootstrap.js', entryPoint);
-  // Copy the publicpath file
-  const publicPath = `./${name}/build/publicpath.js`;
-  fs.copySync('publicpath.js', publicPath);
-  allEntryPoints[`${name}/bundle`] = entryPoint;
-  allEntryPoints[`${name}/publicpath`] = publicPath;
-}
-
-// ! this was used to populate the template
-// Handle the extensions.
-// const { mimeExtensions, plugins } = topLevelData.jupyterlabz;
-
-// ! this happens in createShared() now
-// // Create the list of extension packages from the package.json metadata
-// const extensionPackages = new Set();
-// Object.keys(plugins).forEach((page) => {
-//   const pagePlugins = plugins[page];
-//   Object.keys(pagePlugins).forEach((name) => {
-//     extensionPackages.add(name);
-//   });
-// });
-
-// Create the entry point and other assets in build directory.
-// const source = fs.readFileSync('index.template.js').toString();
-// const extData = {
-//   notebook_plugins: plugins,
-//   notebook_mime_extensions: mimeExtensions,
-// };
-// const indexOut = template(extData);
-const template = Handlebars.compile(
-  fs.readFileSync('index.template.js').toString()
-);
-fs.writeFileSync(
-  path.join(buildDir, 'index.js'),
-  template({
-    notebook_plugins: plugins,
-    notebook_mime_extensions: mimeExtensions,
-  })
-);
-
-// Copy extra files
-const cssImports = path.resolve(__dirname, 'style.js');
-fs.copySync(cssImports, path.resolve(buildDir, 'extraStyle.js'));
-
-const extras = Build.ensureAssets({
-  packageNames: names,
-  output: buildDir,
-  schemaOutput: path.resolve(__dirname, '..', 'notebook'),
-});
-
 // Make a bootstrap entrypoint
 const entryPoint = path.join(buildDir, 'bootstrap.js');
 const bootstrap = 'import("./index.js");';
@@ -350,7 +270,7 @@ module.exports = [
           name: ['_JUPYTERLAB', 'CORE_LIBRARY_FEDERATION'],
         },
         name: 'CORE_FEDERATION',
-        shared: createShared(topLevelData),
+        shared: createShared(data),
       }),
     ],
   }),
